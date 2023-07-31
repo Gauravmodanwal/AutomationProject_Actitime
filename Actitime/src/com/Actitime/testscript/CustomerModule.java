f687c07dd5eed6a414d0fb6194b2ecb0343742a9
Mkgir <Mkgir@DESKTOP-11OMJVP>
2023-07-31 22:13:53 +0530

    CustomerModule

--- Actitime/src/com/Actitime/testscript/CustomerModule.java
+++ Actitime/src/com/Actitime/testscript/CustomerModule.java
@@ -65,5 +65,37 @@ public class CustomerModule extends BaseClass {
 		Assert.assertEquals(actualname, projectname);
 	}
 	
+	@Test
+	public void testCreateTask() throws EncryptedDocumentException, IOException, InterruptedException {
+		Reporter.log("CreateTask",true);
+		Filelib f=new Filelib();
+		String customername = f.getExcelData("CreateCustomer", 1,3);
+		String projectname = f.getExcelData("CreateProject", 1,3);
+		String taskname = f.getExcelData("CreateTask", 1,3);
+		String description = f.getExcelData("CreateTask", 1, 4);
+		String taskname1 = f.getExcelData("CreateProject", 1, 1);
+		EnterTimeTrackPage e=new EnterTimeTrackPage(driver);
+		e.setTasks();
+		TaskListPage task=new TaskListPage(driver);
+		task.getAddNewBtn().click();
+		task.getNewTask().click();
+		Thread.sleep(1000);
+		task.getsCusDDclick().click();
+		Thread.sleep(1000);
+		task.getPassCusNameinTask().sendKeys("Big Bang Company");
+		task.getSelectCus().click();
+		task.getsProjectDD().click();
+		Thread.sleep(1000);
+		task.getPassprojectname().sendKeys(projectname);
+		Thread.sleep(1000);
+		task.getSelectProject();
+		task.getEnterTaskName().sendKeys(taskname);
+		task.getCreateTask().click();
+		String actualTask = task.getTaskcreated().getText();
+		Thread.sleep(2000);
+		
+		
+	}
+	
 	
 }
